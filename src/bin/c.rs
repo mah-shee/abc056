db2261896065dad50203cb3f5cb474bc75a45147
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-19 03:17:37 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,11 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        x: usize,
     }
+    let mut i = 0;
+    while i * (i + 1) / 2 < x {
+        i += 1;
+    }
+    println!("{}", i);
 }
